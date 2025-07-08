b61138ed5a250b65a9b59494dcbdc43fb855984d
donnabell-s <donnasembrano03@gmail.com>
2025-07-08 20:18:56 +0800

    postman-working

--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,19 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ReviewController;
+use Illuminate\Http\Request;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', fn(\Illuminate\Http\Request $request) => $request->user());
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('books', BookController::class);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', action: [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware(['auth:sanctum', 'check.token.expiry'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::apiResource('authors', AuthorController::class);
+    Route::apiResource('books', BookController::class);
     Route::apiResource('genres', GenreController::class);
     Route::apiResource('reviews', ReviewController::class);
 });
-
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
